7ddb705dfec4e55115f08d14e4da92c2fb02ccb3
John Ortiz Ordoñez <johnortizo@outlook.com>
2025-01-04 15:01:38 -0500

    Función para particionar el texto en partes

--- backend/app/utils/utils.py
+++ backend/app/utils/utils.py
@@ -1,6 +1,7 @@
 from io import BytesIO
 
 from PyPDF2 import PdfReader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 def es_pdf_valido(tipo_contenido: str) -> bool:
@@ -35,3 +36,21 @@ def extraer_texto_desde_pdf(pdf: bytes) -> str:
         return text
     except Exception as e:
         raise ValueError(f"Error al leer el PDF: {e}")
+
+
+def particionar_texto_partes(texto, tamanio_parte=1000, parte_empate=200):
+    """
+    Particiona un texto en partes más pequeñas.
+
+    Args:
+    texto (str): El texto a particionar.
+    tamanio_parte (int): El tamaño de cada parte.
+    parte_empate (int): La cantidad de caracteres que se superponen entre cada parte.
+
+    Returns:
+    List[str]: Una lista con las partes del texto.
+    """
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=tamanio_parte, chunk_overlap=parte_empate
+    )
+    return splitter.split_text(texto)

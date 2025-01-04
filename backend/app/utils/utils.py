a9ea615afb601c35efebdc9bd534bcfb5e10217d
John Ortiz Ordoñez <johnortizo@outlook.com>
2025-01-04 07:32:43 -0500

    Función utilitaria para extraer el texto de un PDF

--- backend/app/utils/utils.py
+++ backend/app/utils/utils.py
@@ -1,3 +1,8 @@
+from io import BytesIO
+
+from PyPDF2 import PdfReader
+
+
 def es_pdf_valido(tipo_contenido: str) -> bool:
     """
     Valida si el archivo es un PDF.
@@ -9,3 +14,24 @@ def es_pdf_valido(tipo_contenido: str) -> bool:
     bool: True si el archivo es un PDF, False en caso contrario.
     """
     return tipo_contenido == "application/pdf"
+
+
+def extraer_texto_desde_pdf(pdf: bytes) -> str:
+    """
+    Extrae el texto de un archivo PDF.
+
+    Args:
+    pdf (bytes): El contenido del archivo PDF.
+
+    Returns:
+    str: El texto extraído del archivo
+    """
+    try:
+        pdf_reader = PdfReader(BytesIO(pdf))
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        
+        return text
+    except Exception as e:
+        raise ValueError(f"Error al leer el PDF: {e}")

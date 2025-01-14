562fa13bceb56a422b9fba691e07878591af1267
John Ortiz Ordoñez <johnortizo@outlook.com>
2025-01-14 06:38:46 -0500

    Plantilla inicial para el componente de lista de preguntas

---
+++ frontend/src/components/ListaPreguntas.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+      <div v-for="(question, index) in questions" :key="index">
+        <PreguntaInput
+          v-model="questions[index]"
+          @remove="removeQuestion(index)"
+        />
+      </div>
+      <button
+        @click="addQuestion"
+        class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mb-4"
+      >
+        + Agregar pregunta
+      </button>
+    </div>
+  </template>
+  
+  <script>
+  import PreguntaInput from "./PreguntaInput.vue";
+  
+  export default {
+    components: { PreguntaInput },
+    props: ["value"],
+    data() {
+      return {
+        questions: this.value || [""],
+      };
+    },
+    watch: {
+      questions: {
+        deep: true,
+        handler(newVal) {
+          this.$emit("update:value", newVal);
+        },
+      },
+    },
+    methods: {
+      addQuestion() {
+        this.questions.push("");
+      },
+      removeQuestion(index) {
+        this.questions.splice(index, 1);
+      },
+    },
+  };
+  </script>
+  

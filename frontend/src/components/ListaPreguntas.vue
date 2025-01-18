2594008262ccd01ed2156b4d89f80592babc58e0
John Ortiz Ordoñez <johnortizo@outlook.com>
2025-01-18 05:23:31 -0500

    Intento de vincular o pasar datos entre componentes

--- frontend/src/components/ListaPreguntas.vue
+++ frontend/src/components/ListaPreguntas.vue
@@ -1,49 +1,40 @@
 <template>
-    <div>
-      <div v-for="(question, index) in questions" :key="index">
-        <PreguntaInput
-          v-model="questions[index]"
-          @remove="removeQuestion(index)"
-        />
-      </div>
-      <button
-        @click="addQuestion"
-        class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mb-4 w-full"
-      >
-      <i class="fas fa-plus"></i> Agregar pregunta
-      </button>
+  <div>
+    <div v-for="(question, index) in questions" :key="index">
+      <PreguntaInput v-model="questions[index]" @remove="removeQuestion(index)" />
     </div>
-  </template>
-  
-  <script>
-  import PreguntaInput from "./PreguntaInput.vue";
-  
-  export default {
-    components: { PreguntaInput },
-    props: ["value"],
-    data() {
-      return {
-        questions: this.value || [""],
-      };
-    },
-    watch: {
-      questions: {
-        deep: true,
-        handler(newVal) {
-          console.log('ListaPreguntas > newVal', newVal);
-          
-          this.$emit("update:value", newVal);
-        },
+    <button @click="addQuestion" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mb-4 w-full">
+      <i class="fas fa-plus"></i> Agregar pregunta
+    </button>
+  </div>
+</template>
+
+<script>
+import PreguntaInput from "./PreguntaInput.vue";
+
+export default {
+  components: { PreguntaInput },
+  props: ["value"],
+  data() {
+    return {
+      questions: this.value || [""],
+    };
+  },
+  watch: {
+    questions: {
+      deep: true,
+      handler(newVal) {
+        this.$emit("update:value", newVal);
       },
     },
-    methods: {
-      addQuestion() {
-        this.questions.push("");
-      },
-      removeQuestion(index) {
-        this.questions.splice(index, 1);
-      },
+  },
+  methods: {
+    addQuestion() {
+      this.questions.push("");
     },
-  };
-  </script>
-  
+    removeQuestion(index) {
+      this.questions.splice(index, 1);
+    },
+  },
+};
+</script>
--- frontend/src/components/PreguntaInput.vue
+++ frontend/src/components/PreguntaInput.vue
@@ -1,36 +1,31 @@
 <template>
-    <div class="flex items-center gap-2 mb-4">
-      <input
-        v-model="questionText"
-        type="text"
-        placeholder="Escribe tu pregunta..."
-        class="flex-1 border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring focus:ring-blue-500"
-        @input="validateInput"
-      />
-      <button
-        @click="$emit('remove')"
-        class="bg-red-500 text-white px-3 py-2 rounded hover:bg-red-600"
-      >
+  <div class="flex items-center gap-2 mb-4">
+    <input v-model="questionText" type="text" placeholder="Escribe tu pregunta..."
+      class="flex-1 border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring focus:ring-blue-500"
+      @input="validateInput" />
+    <button @click="$emit('remove')" class="bg-red-500 text-white px-3 py-2 rounded hover:bg-red-600">
       <i class="fas fa-trash-alt"></i>
-      </button>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    props: ["value"],
-    data() {
-      return {
-        questionText: this.value || "",
-      };
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  data() {
+    return {
+      questionText: this.value || "",
+    };
+  },
+  watch: {
+    questionText(newVal) {
+      this.$emit("update:value", newVal);
+    }
+  },
+  methods: {
+    validateInput() {
+      this.$emit("update:value", this.questionText);
     },
-    methods: {
-      validateInput() {
-        console.log('PreguntaInput > this.questionText', this.questionText);
-        
-        this.$emit("update:value", this.questionText);
-      },
-    },
-  };
-  </script>
-  
+  },
+};
+</script>
